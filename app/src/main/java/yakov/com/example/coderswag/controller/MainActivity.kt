6b5c1fc631cb95a40848a1dc934b9aa4b8260bbd
Yakov Golovin <yakov.golovin@gmail.com>
2020-02-15 13:18:32 +0100

    Lesson 2

--- app/src/main/java/yakov/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/yakov/com/example/coderswag/controller/MainActivity.kt
@@ -2,12 +2,27 @@ package yakov.com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import yakov.com.example.coderswag.R
+import yakov.com.example.coderswag.model.Category
+import yakov.com.example.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListVIew.adapter = adapter
     }
 }
