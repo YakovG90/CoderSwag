0f5cab349ac72a826e24d1851a697b847829432c
Yakov Golovin <yakov.golovin@gmail.com>
2020-02-15 14:44:02 +0100

    Prepare for recycler view adapter

--- app/src/main/java/yakov/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/yakov/com/example/coderswag/controller/MainActivity.kt
@@ -1,14 +1,10 @@
 package yakov.com.example.coderswag.controller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Adapter
-import android.widget.ArrayAdapter
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import yakov.com.example.coderswag.R
 import yakov.com.example.coderswag.adapters.CategoryAdapter
-import yakov.com.example.coderswag.model.Category
 import yakov.com.example.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
